ca9ef4e4ed738064ec722ec7b6ac1d8d23d354d0
Joaquin Sanchez <yoa192@gmail.com>
2022-07-04 19:34:10 -0400

    modificacion de dom con simulador de credito

--- dom.js
+++ dom.js
@@ -8,7 +8,7 @@ autos = [{
         marca: "volkswagen",
         modelo: "virtus",
         precioCredito: 15000000,
-        "precioLista": 18000000
+        precioLista: 18000000
     },
     {
         marca: "chevrolet",
@@ -21,6 +21,24 @@ autos = [{
         modelo: "f150",
         precioCredito: 35000000,
         precioLista: 39000000
+    },
+    {
+        marca: "MG",
+        modelo: "GT",
+        precioCredito: 13300000,
+        precioLista: 15000000
+    },
+    {
+        marca: "MG",
+        modelo: "ZS",
+        precioCredito: 11500000,
+        precioLista: 13000000
+    },
+    {
+        marca: "MG",
+        modelo: "3",
+        precioCredito: 9700000,
+        precioLista: 11000000
     },
     {
         marca: "fiat",
@@ -59,12 +77,11 @@ marcaElegida.addEventListener("change", cambioMarca)
 function cambioMarca() {
 
     let filtroMarca = autos.filter(marca => marca.marca === marcaElegida.value)
+    let listaModelo = document.querySelector(`#selectModelo`);
+    listaModelo.innerHTML = `<option value="" disabled selected>-- Seleccione --</option>`
 
     for (const filtrado of filtroMarca) {
-
-        let listaModelo = document.querySelector("#selectModelo")
         let nuevoAuto = document.createElement("option");
-        nuevoAuto.value = filtrado.modelo;
         nuevoAuto.innerHTML = filtrado.modelo;
         listaModelo.append(nuevoAuto);
     }
@@ -72,50 +89,49 @@ function cambioMarca() {
 
 let modeloElegido = document.getElementById('selectModelo');
 
-console.log(modeloElegido.value)
-
-modeloElegido.addEventListener("change", cambioModelo)
+modeloElegido.addEventListener("change", cambioModelo);
 
 function cambioModelo() {
+
 }
 
 let radioBtns = document.querySelectorAll("input[name='pago']");
 
 let encotrarSelect = () => {
     let select = document.querySelector("input[name='pago']:checked").value;
+    let filtroAuto = autos.find(auto => auto.marca === marcaElegida.value && auto.modelo === modeloElegido.value)
+    let div = document.querySelector(`.resultado`)
+    div.innerHTML = ""
     if(select === "contado"){
-        let filtroAuto = autos.find(auto => auto.marca === marcaElegida.value && auto.modelo === modeloElegido.value)
-        let formulario = document.getElementById("form");
         let fieldset = document.createElement("fieldset");
-        let legend = document.createElement("legend");
-        legend.innerHTML = "Pago al CONTADO";
-        let elegido = document.createElement("h4");
-        let texto = document.createElement("p");
-        elegido.innerText = "el auto que elegiste es: "+ marcaElegida.value + " " + modeloElegido.value;
-        texto.innerHTML = "el monto que tienes que pagar es de: $"+ filtroAuto.precioLista ;
-        formulario.append(fieldset);
-        fieldset.append(legend);
-        fieldset.append(elegido);
-        fieldset.append(texto);
-        
+        fieldset.innerHTML = `<legend class="reset">Pago al CONTADO</legend>
+        <h4>el auto que elegiste es: ${marcaElegida.value} ${modeloElegido.value}</h4>
+        <p>el monto que tienes que pagar es de: $${filtroAuto.precioLista}</p>`;
+        div.append(fieldset);
     }else{
-        let filtroAuto = autos.find(auto => auto.marca === marcaElegida.value && auto.modelo === modeloElegido.value)
-        let formulario = document.getElementById("form");
         let fieldset = document.createElement("fieldset");
-        let legend = document.createElement("legend");
-        legend.innerHTML = "Cotizando el CREDITO";
-        let montoLabel = document.createElement("label")
-        let montoInput = document.createElement("input")
-        montoInput.type = Number
-        formulario.append(fieldset);
-        fieldset.append(legend);
-        fieldset.append(montoInput);
-
-
-        console.log("pago credito")
+        fieldset.id = "cotizador"
+        fieldset.innerHTML = `<legend class="reset">Cotizando el CREDITO</legend><label for="pie">Pie a Pagar</label>
+        <input type="number" name="pie" id="pie" min="${(filtroAuto.precioCredito)*0.1}" max="${(filtroAuto.precioCredito)*0.5}" required="true"><label for="cuotas">Cantidad de cuotas</label>
+        <input type="number" required name="cuotas" id="cuotas" min="${24}" max="${60}">`;
+        div.append(fieldset);
     }
 }
 
 radioBtns.forEach(radioBtn => {
     radioBtn.addEventListener("change", encotrarSelect)
 })
+
+let form = document.querySelector("#form")
+
+let btnClick = event => {
+    let filtroAuto = autos.find(auto => auto.marca === marcaElegida.value && auto.modelo === modeloElegido.value)
+    let monto = filtroAuto.precioCredito - pie.value
+    let prestamo = (monto*cuotas.value*0.018) + monto;
+    let cuotaMensual = Math.round(prestamo/cuotas.value);
+    alert(`el valor del credito es de $${prestamo}
+y las cuotas mensuales son de $${cuotaMensual}`);
+    event.preventDefault();
+}
+
+form.addEventListener("submit", btnClick);
